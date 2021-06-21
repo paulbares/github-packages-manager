bc0cff26f57489121ceb7ad8ce4d6dc631f4e9d2
paul <bares.paul@gmail.com>
2021-06-21 17:05:05 +0400

    Clean code

--- src/main.js
+++ src/main.js
@@ -1,19 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueMaterial from 'vue-material'
-// import { MdButton, MdField, MdDialogConfirm, MdDialogTitle } from 'vue-material/dist/components'
+
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
 
 Vue.config.productionTip = false
 
 Vue.use(VueMaterial)
-// Vue.use(MdButton)
-// Vue.use(MdField)
-// Vue.use(MdDialogConfirm)
-// Vue.use(MdDialogTitle)
-// Vue.use(MdDialogActions)
-// Vue.use(MdDialogContent)
 
 new Vue({
   render: h => h(App),
